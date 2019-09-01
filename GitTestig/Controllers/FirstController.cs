33c2385959c360ef98aec61f6b608adba9ea5be9
Abubakar <mianabubakar131998@gmail.com>
2019-09-01 11:52:58 +0500

    Add AboutUs Action in FirstController

--- GitTestig/Controllers/FirstController.cs
+++ GitTestig/Controllers/FirstController.cs
@@ -12,5 +12,9 @@ namespace GitTestig.Controllers
         {
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
     }
 }
